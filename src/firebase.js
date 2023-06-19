8d22b411b52b7637c9d2c08da84b80dc4b2f2b59
Leah Boyd <leahboyd@Leahs-MacBook-Air.local>
2023-06-19 11:00:28 -0400

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { FIREBASE_CONFIG } from "../config";
+const firebaseConfig = FIREBASE_CONFIG
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+export { db }
