536a863239b84eff5c1597423bc524c7366997ac
Leah Boyd <leahboyd@Leahs-MacBook-Air.local>
2023-06-19 11:01:50 -0400

    moved config file

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-import { FIREBASE_CONFIG } from "../config";
+import { FIREBASE_CONFIG } from "./config";
 const firebaseConfig = FIREBASE_CONFIG
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
